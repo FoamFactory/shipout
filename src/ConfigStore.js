b1a6f43d76802645f620616b2060a70f78181906
Scott Johnson <jaywir3@gmail.com>
2020-06-13 09:33:27 -0500

    :package: Make configurations individual per-variable.
    Previously, if the ENTIRE configuration couldn't be loaded either from
    package.json or from environment variables, the script would report an
    error. This changes the behavior so that each individual variable can
    either come from an environment variable OR from the package.json file.

--- src/ConfigStore.js
+++ src/ConfigStore.js
@@ -9,11 +9,7 @@ import * as process from 'process';
 export class ConfigStore {
   constructor(projectPath) {
     this.projectPath = projectPath;
-    this.configuration = this.getConfigurationFromPackageJson();
-
-    if (!this.configuration) {
-      this.configuration = this.getConfigFromEnvironment();
-    }
+    this.configuration = this.getConfiguration();
   }
 
   getAppEnvironment() {
@@ -48,40 +44,49 @@ export class ConfigStore {
     return this.projectPath;
   }
 
-  getConfigurationFromPackageJson() {
-    const pkg = this._getPackageConfig();
+  getConfiguration() {
+    let app_environment = this.getVariableFromPackageJson("app_environment");
+    let deploy_base_dir = this.getVariableFromPackageJson("deploy_base_dir");
+    let deploy_server = this.getVariableFromPackageJson("deploy_server");
+    let deploy_user = this.getVariableFromPackageJson("deploy_user");
 
-    if (pkg.shipout && pkg.shipout.app_environment
-          && pkg.shipout.deploy_base_dir && pkg.shipout.deploy_server
-          && pkg.shipout.deploy_user) {
-      return {
-        "app_environment": pkg.shipout.app_environment,
-        "deploy_base_dir": pkg.shipout.deploy_base_dir,
-        "deploy_server": pkg.shipout.deploy_server,
-        "deploy_user": pkg.shipout.deploy_user
-      }
-    }
+    let retVal =  {
+      "app_environment": app_environment != null ? app_environment
+        : this.getVariableFromEnvironment("APP_ENVIRONMENT"),
+      "deploy_base_dir": deploy_base_dir != null ? deploy_base_dir
+        : this.getVariableFromEnvironment("DEPLOY_BASE_DIR"),
+      "deploy_server": deploy_server != null ? deploy_server
+        : this.getVariableFromEnvironment("DEPLOY_SERVER"),
+      "deploy_user": deploy_user != null ? deploy_user
+        : this.getVariableFromEnvironment("DEPLOY_USER")
+    };
 
-    return null;
-  }
-
-  getConfigFromEnvironment() {
-    // Verify we have the correct environment variables to continue
-    let requiredVars = [ 'APP_ENVIRONMENT', 'DEPLOY_BASE_DIR', 'DEPLOY_SERVER',
-                         'DEPLOY_USER' ];
+    let requiredVars = [ "app_environment", "deploy_base_dir", "deploy_server",
+                         "deploy_user" ];
 
     for (let requiredVarsIdx in requiredVars) {
-      if (!process.env.hasOwnProperty(requiredVars[requiredVarsIdx])) {
-        throw `The environment variable ${requiredVars[requiredVarsIdx]} was not specified`;
+      if (!retVal[requiredVars[requiredVarsIdx]]
+          || retVal[requiredVars[requiredVarsIdx]] == null) {
+        throw "Neither a package.json configuration nor an environment "
+              + `variable was specified for ${requiredVars[requiredVarsIdx]}`;
       }
     }
 
-    return {
-      "app_environment": process.env.APP_ENVIRONMENT,
-      "deploy_base_dir": process.env.DEPLOY_BASE_DIR,
-      "deploy_server": process.env.DEPLOY_SERVER,
-      "deploy_user": process.env.DEPLOY_USER
+    return retVal;
+  }
+
+  getVariableFromPackageJson(varName) {
+    const pkg = this._getPackageConfig();
+
+    if (!pkg.shipout) {
+      return null;
     }
+
+    return pkg.shipout[varName];
+  }
+
+  getVariableFromEnvironment(varName) {
+    return process.env[varName];
   }
 
   _getPackageConfig() {
